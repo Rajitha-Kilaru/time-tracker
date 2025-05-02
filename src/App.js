5ba6456c07b96aee5495f875177efbf69487ee18
rajitha.chowdary <rajitha.chowdary@cognitivzen.com>
2025-05-02 12:15:36 +0530

    test change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React welcome
+          Tracking employee attendance with React
         </a>
       </header>
     </div>
